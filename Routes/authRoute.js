11607cf6f26a248a80a3e7c64edb7a7120500a46
Bootstrap02 <udegbue69@gmail.com>
2024-01-05 06:25:38 +0100

    Campusbuy authentication staying signed in

--- Routes/authRoute.js
+++ Routes/authRoute.js
@@ -5,7 +5,6 @@ const users = require('../Controllers/usersCtrl.js');
 router.post('/', users.createUser);
 router.get('/',  users.getAllUsers);
 router.put('/:id', users.updateAUser);
-router.put('/:id', users.updatePassword);
 router.delete('/:id', users.deleteAUser);
 router.get('/:id',  users.getAUser);
 
